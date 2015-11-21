0f03dcec953e6994c2de66182255b1454f84d27b
Artem Lyubutov <lubytov@me.com>
2015-11-22 04:15:48 +0500

    adding :id page

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 var express = require('express');
-app = express();
+var app = express();
 //это метод heroku, чтобы установить нужный порт
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Go for for lunch',
+	completed: false,
+}, {
+	id: 2,
+	description: 'Buy game', 
+	completed: false
+}, {
+	id: 3,
+	description: 'Study node.js',
+	completed: true
+}];
 
 app.get('/', function (req, res) {
 	res.send('<h1>ToDo API Root</h1>');
 });
 
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) { // нужно чтобы было именно :id 
+	var todoId = parseInt(req.params.id, 10); // присваиваем переменной значение которое пользователь ввел в строку и преображаем в число
+	var matchedTodo; // хранилище для совпадения
+
+	todos.forEach(function (todo) { // ищем совпадение 
+		if (todo.id === todoId) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) { // если нашли, то отправляем респонс
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send(); // если нет, то отправляем 404
+	}
+});
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
-});
+});	
